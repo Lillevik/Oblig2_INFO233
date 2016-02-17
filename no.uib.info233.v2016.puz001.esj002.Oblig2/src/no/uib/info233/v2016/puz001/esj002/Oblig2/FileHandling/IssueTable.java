a717fc2c2232cbf210032e0ab23bc040e9dcb5d2
Lillevik <mariusfalch@hotmail.com>
2016-02-17 11:16:38 +0100

    Moved tableForIssues()
    Moved tableForIssues into the class IssueTable.

--- no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/FileHandling/IssueTable.java
+++ no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/FileHandling/IssueTable.java
@@ -3,7 +3,17 @@
  */
 package no.uib.info233.v2016.puz001.esj002.Oblig2.FileHandling;
 
+import java.io.File;
+
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * @author mariuslillevik
@@ -11,36 +21,64 @@ import javax.swing.JTable;
  */
 public class IssueTable extends JTable{
 	
-	
-	
-	
-
 
 	/**
 	 * 
 	 */
 		private static final long serialVersionUID = 1L;
-		private JTable issues = new JTable();
+		private DefaultTableModel model = new DefaultTableModel();
 		
 		public IssueTable(){
+			tableForIssues();
 		}
 	
 
+		
+	    public void tableForIssues(){
+	    	
+	      	try {
+	      	File fXmlFile = new File("old_issues.xml");
+	    			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	    			Document doc = dBuilder.parse(fXmlFile);
+	    			doc.getDocumentElement().normalize();
+	    	        model.addColumn("Issue ID: ");
+	    	        model.addColumn("Assigned to: ");
+	    	        model.addColumn("Created: ");
+	    	        model.addColumn("Issue: ");
+	    	        model.addColumn("Priority: ");
+	    	        model.addColumn("Location: ");
+
+	      NodeList nodelist1 = doc.getElementsByTagName("ISSUES");
+	      for (int i = 0; i < nodelist1.getLength(); i++)
+	      {
+	          Node node = nodelist1.item(i);
+	          Element eElement = (Element) node;
+	          model.addRow(new Object[]{eElement.getAttribute("id"), eElement.getAttribute("assigned_user"), eElement.getAttribute("created"), eElement.getAttribute("text"), eElement.getAttribute("priority"), eElement.getAttribute("location")});
+	      }
+	      	}
+	   			
+	   		    catch (Exception e) {
+	   			e.printStackTrace();
+	   		    }
+	   	 
+	   	}
+	    
 
 	/**
-	 * @return the issues
+	 * @return the model
 	 */
-	public JTable getIssues() {
-		return issues;
+	public DefaultTableModel getModel() {
+		return model;
 	}
 
 
 
 	/**
-	 * @param issues the issues to set
+	 * @param model the model to set
 	 */
-	public void setIssues(JTable issues) {
-		this.issues = issues;
+	public void setModel(DefaultTableModel model) {
+		this.model = model;
 	}
 
 
--- no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/Gui/Gui.java
+++ no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/Gui/Gui.java
@@ -1,19 +1,9 @@
 package no.uib.info233.v2016.puz001.esj002.Oblig2.Gui;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import no.uib.info233.v2016.puz001.esj002.Oblig2.FileHandling.IssueTable;
 
 import java.awt.*;
-import java.io.File;
 
 /**
  * Created by esj002 and puz001
@@ -32,23 +22,21 @@ public class Gui extends JFrame {
     private JTextField txtSearch;
     private JLabel searchLabel;
     private JTextPane txtInfo;
-    private DefaultTableModel model = new DefaultTableModel();
-    private JTable qTable = new JTable(model);
+    private IssueTable it = new IssueTable();
+//    private DefaultTableModel model;
+    private JTable qTable = new JTable(it.getModel());
     
     /**
      * Constructor for the Gui class which extends from JFrame.
      */
     public Gui(){
     	super("Issue Tracker");
-
-		tableForIssues();
         spine = new JPanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		spine.setLayout(new BorderLayout(500, 150));
 		setContentPane(spine);
 		setupComponents();
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-
 		pack();
 		setVisible(true);
     }
@@ -92,14 +80,14 @@ public class Gui extends JFrame {
         txtInfo.setText("info field");
         txtInfo.setPreferredSize(new Dimension(190, 500));
         txtInfo.setBackground(Color.gray);
+        	
+        
 
-        	qTable.setPreferredSize(new Dimension(540, 640));
         	qTable.setBackground(Color.white);
         	qTable.getAutoResizeMode();
-        
-//        qTable.setFillsViewportHeight(true);
-//        JScrollPane scrollPane = new JScrollPane(qTable);
-//        scrollPane.add(qTable);
+        	qTable.setFillsViewportHeight(true);
+        	
+
 
         //Adds the components to the Panels
         spine.add(panelBackRight, BorderLayout.CENTER);
@@ -114,39 +102,7 @@ public class Gui extends JFrame {
     
     
     
-    public void tableForIssues(){
-    	
-      	try {
-      	File fXmlFile = new File("old_issues.xml");
-    			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    			Document doc = dBuilder.parse(fXmlFile);
-    			doc.getDocumentElement().normalize();
-    	        model.addColumn("Issue ID: ");
-    	        model.addColumn("Assigned to: ");
-    	        model.addColumn("Created: ");
-    	        model.addColumn("Issue: ");
-    	        model.addColumn("Priority: ");
-    	        model.addColumn("Location: ");
 
-      NodeList nodelist1 = doc.getElementsByTagName("ISSUES");
-      for (int i = 0; i < nodelist1.getLength(); i++)
-      {
-          Node node = nodelist1.item(i);
-          Element eElement = (Element) node;
-          model.addRow(new Object[]{eElement.getAttribute("id"), eElement.getAttribute("assigned_user"), eElement.getAttribute("created"), eElement.getAttribute("text"), eElement.getAttribute("priority"), eElement.getAttribute("location")});
-          
-          
-
-      }
-      	}
-   			
-   		    catch (Exception e) {
-   			e.printStackTrace();
-   		    }
-   	 
-   	}
-    
 
 	/**
 	 * @return the panelBackRight
@@ -271,23 +227,5 @@ public class Gui extends JFrame {
 	 */
 	public void setqTable(JTable qTable) {
 		this.qTable = qTable;
-	}
-
-
-
-	/**
-	 * @return the model
-	 */
-	public DefaultTableModel getModel() {
-		return model;
-	}
-
-
-
-	/**
-	 * @param model the model to set
-	 */
-	public void setModel(DefaultTableModel model) {
-		this.model = model;
 	}
 }
