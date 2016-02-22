fc7637bafd3279623c11bdd5cdc841d09b4d1b5e
svimanet <esj002@student.uib.no>
2016-02-22 12:40:43 +0100

    ny knapp

--- no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/Gui/Gui.java
+++ no.uib.info233.v2016.puz001.esj002.Oblig2/src/no/uib/info233/v2016/puz001/esj002/Oblig2/Gui/Gui.java
@@ -21,7 +21,8 @@ public class Gui extends JFrame {
     
     //Jbuttons
     private JButton btnSearch;
-    private JButton listAllIssues;
+    private JButton btnListAllIssues;
+	private JButton btnListAllUsers;
     
     //JTextFields 
     private JTextField txtSearch;
@@ -53,10 +54,8 @@ public class Gui extends JFrame {
 		pack();
 		setVisible(true);
     }
-    
-    
-    
-    /**
+
+	/**
      * Initializes components and
      * sets them up with custom designs.
      */
@@ -93,12 +92,15 @@ public class Gui extends JFrame {
         txtSearch.setPreferredSize(new Dimension(190, 20));
 
         //Sets up the JButton btnSearch
-        btnSearch = new JButton("List employees");
+        btnSearch = new JButton("Search");
         btnSearch.setSize(new Dimension( 20, 20));
         
         //Sets up the JButton listAll
-        listAllIssues = new JButton("List all Issues");
-        listAllIssues.setSize(new Dimension( 20, 20));
+        btnListAllIssues = new JButton("List Issues");
+        btnListAllIssues.setSize(new Dimension( 20, 20));
+
+		btnListAllUsers = new JButton("List users");
+		btnListAllUsers.setSize(new Dimension(20, 20));
 
         //Sets up the JLabel txtInfo
         txtInfo = new JTextPane();
@@ -113,7 +115,9 @@ public class Gui extends JFrame {
         panelBackLeft.add(searchLabel);
         panelBackLeft.add(txtSearch);
         panelBackLeft.add(btnSearch);
-        panelBackLeft.add(listAllIssues);
+
+		panelBackLeft.add(btnListAllUsers);
+        panelBackLeft.add(btnListAllIssues);
         panelBackLeft.add(txtInfo);
         panelMidTopLeft.add(new JScrollPane(qTable));
     }
@@ -310,18 +314,22 @@ public class Gui extends JFrame {
 
 
 	/**
-	 * @return the listAllIssues
+	 * @return the btnListAllIssues
 	 */
-	public JButton getListAllIssues() {
-		return listAllIssues;
+	public JButton getBtnListAllIssues() {
+		return btnListAllIssues;
+	}
+
+	public JButton getBtnListAllUsers() {
+		return btnListAllUsers;
 	}
 
 
 
 	/**
-	 * @param listAllIssues the listAllIssues to set
+	 * @param btnListAllIssues the btnListAllIssues to set
 	 */
-	public void setListAllIssues(JButton listAllIssues) {
-		this.listAllIssues = listAllIssues;
+	public void setListAllIssues(JButton btnListAllIssues) {
+		this.btnListAllIssues = btnListAllIssues;
 	}
 }
